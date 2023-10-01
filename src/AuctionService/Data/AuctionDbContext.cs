146c9402ad2fb7ac6ceb7399ec71ab10112f6293
Vitalii Derda <derdavitaliy@gmail.com>
2023-10-01 10:41:46 +0300

    feat: added auction service

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuctionService.Entities;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions<AuctionDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+}
---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(destinationMember => destinationMember.Item,
+                memberOptions =>
+                    memberOptions.MapFrom(mapExpression => mapExpression));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
