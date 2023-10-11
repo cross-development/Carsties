674f91e30e4747ee8630af205c7857e19a93d954
Vitalii Derda <derdavitaliy@gmail.com>
2023-10-11 17:45:34 +0300

    feat: added mq outbox

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -18,6 +18,12 @@ builder.Services.AddMassTransit(configs =>
     configs.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
     configs.UsingRabbitMq((context, cfg) =>
     {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
